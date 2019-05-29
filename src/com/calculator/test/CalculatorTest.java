0775d09d5a1d9a95d04f59f64ec303e4dcda2950
seungin yang <rhfpdk92@naver.com>
2019-05-29 15:01:14 +0900

    @Before, @After 어노테이션으로 전처리 후처리 작업 가능

--- src/com/calculator/test/CalculatorTest.java
+++ src/com/calculator/test/CalculatorTest.java
@@ -2,17 +2,38 @@ package com.calculator.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.junit.Calculator;
 
 public class CalculatorTest {
 
+//	private Calculator cal = new Calculator(); //추천하지않
+	private Calculator cal; 
+
+	//추천 , 매번 테스트가 서로간 영향을 미치지 않으면서 독립적으로 실행된다는것을 보장함 //초기화작업과 후처리작
+	@Before
+	public void setup() {
+		cal = new Calculator();
+		System.out.println("Setupt!");
+	}
 	@Test
-	public void testMultiple() {
-		//fail("Not yet implemented");
-		Calculator cal = new Calculator();
-		assertEquals(63, cal.multiple(7, 9));
+	public void testAdd() {		
+		assertEquals(3, cal.add(2, 1));
+		System.out.println("add!");
+	}
+
+	@Test
+	public void testdivide() {
+		assertEquals(3, cal.divide(9, 3));
+		System.out.println("divide!");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown!");
 	}
 
 }
--- src/com/junit/Calculator.java
+++ src/com/junit/Calculator.java
@@ -5,4 +5,16 @@ public class Calculator {
 		return num1 * num2;
 	}
 
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+
 }
